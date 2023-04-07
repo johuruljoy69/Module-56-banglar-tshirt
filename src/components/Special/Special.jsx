36acdffc5eee7a1c7e74fc9ee9f9005e7bec08e6
johirjoy <joy.sd69@gmail.com>
2023-04-07 23:49:16 +0600

    update code

--- src/components/Special/Special.jsx
+++ src/components/Special/Special.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useContext } from 'react';
+import { RingContext } from '../Grandpa/Grandpa';
 
 const Special = ({ring}) => {
+    const ring2 = useContext(RingContext);
     return (
         <div>
             <h2>Special</h2>
-            <p><small>Ring: {ring} </small></p>
+            <p><small>Ring: {ring2} </small></p>
         </div>
     );
 };
